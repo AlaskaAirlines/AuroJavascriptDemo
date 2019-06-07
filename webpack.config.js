c84e5af90be253c46412fb59cf9aa3ef0a3140e7
Geoff Rich <Geoff.Rich@alaskaair.com>
2019-06-07 09:10:44 -0700

    Working in IE11

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,33 @@ const path = require('path');
 const uglifyjsWebpackPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    devtool: 'inline-source-map',
-    entry: ['@babel/polyfill', './src/index.js'],
+    entry: ['@babel/polyfill', '@webcomponents/template', './src/index.js'],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules\/(?!(lit-html))/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    modules: false,
+                                    targets: {
+                                        ie: 11,
+                                    },
+                                },
+                            ],
+                        ]
+                    },
+                },
+            },
             {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
