20568801ea347076780c2ab0747232a6f40c4d3a
Geoff Rich <Geoff.Rich@alaskaair.com>
2019-06-07 16:49:05 -0700

    Use recommended babel config

--- webpack.config.js
+++ webpack.config.js
@@ -10,27 +10,57 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [new CompressionPlugin(), new BundleAnalyzerPlugin()],
+    plugins: [new CompressionPlugin(), /*new BundleAnalyzerPlugin()*/],
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules\/(?!(lit-html))/,
+                include: [
+                    // list all locations that need to be babelized for browser support
+                    path.resolve(__dirname, "src"),
+                    path.resolve(__dirname, "node_modules/lit-element"),
+                    path.resolve(__dirname, "node_modules/lit-html"),
+                    path.resolve(__dirname, "node_modules/@alaskaairux/ods-button")
+                  ],
                 use: {
                     loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    modules: false,
-                                    targets: {
-                                        ie: 11,
-                                    },
-                                },
+                    options: {presets: [
+                        [
+                          '@babel/preset-env',
+                          {
+                            useBuiltIns: 'usage', // automatically import polyfills when needed based on "unsupported" feature usage
+                            corejs: '3.0.0',
+                            targets: {
+                              chrome: '70',
+                              edge: '40',
+                              ie: '10',
+                              safari: '9',
+                              firefox: '63',
+                              ios: '11',
+                              android: '7',
+                            },
+                          },
+                        ],
+                        // include other presets here (e.g. @babel/preset-react)
+                      ],
+                      plugins: [
+                        '@babel/plugin-proposal-class-properties', // optional, but nice for making JS classes look *more* like Java or C# classes
+                        '@babel/plugin-transform-spread', // support ES6 spread operators in older browsers (e.g. [ ...subset1, ...subset2 ])
+                        [
+                          '@babel/plugin-proposal-object-rest-spread', // support ES6 object spread operators in older browsers (e.g. { ...object1, ...object2 })
+                          {
+                            useBuiltIns: true,
+                          },
+                        ],
+                        '@babel/plugin-transform-template-literals', // suport ES6 string template literals in older browsers (e.g. `Hello, ${nameVariable}!`)
+                      ],
+                      env: {
+                        test: {
+                          plugins: [
+                            '@babel/plugin-transform-modules-commonjs' // transform `import x from 'y'` syntax to `const x = require('y')` in test environment
                             ],
-                        ]
-                    },
+                        },
+                      }}
                 },
             },
             {
