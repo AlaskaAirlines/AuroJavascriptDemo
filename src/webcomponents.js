7436cf797c9150ca3feeeaf872861273f3c0adde
Dale Sande <dale.sande@alaskaair.com>
2020-07-27 22:15:57 -0700

    feat: update demo to use more components

--- src/webcomponents.js
+++ src/webcomponents.js
@@ -1,5 +1,7 @@
 /* Import any web components used here */
 import '@alaskaairux/ods-button/dist/auro-button';
+import '@alaskaairux/ods-hyperlink/dist/auro-hyperlink';
 import '@alaskaairux/auro-checkbox';
 import '@alaskaairux/auro-checkbox/dist/auro-checkbox-group';
 import '@alaskaairux/ods-toast';
+import "@alaskaairux/auro-header";
